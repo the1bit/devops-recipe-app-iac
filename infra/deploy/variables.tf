8ae9214e8057fd1e005672dd24985c695656fe81
the1bit <tibor.kiss@cloudmentor.hu>
2025-01-03 14:44:06 +0100

    added dns zone, variabkes and outputs

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -35,3 +35,18 @@ variable "django_secret_key" {
   description = "Secret key for Django"
 }
 
+variable "dns_zone_name" {
+  description = "Domain name"
+  default     = "finall.cloud"
+}
+
+variable "subdomain" {
+  description = "Subdomain for each environment"
+  type        = map(string)
+
+  default = {
+    prod    = "api"
+    staging = "api.staging"
+    dev     = "api.dev"
+  }
+}
